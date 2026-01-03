1b783d398e666e6d4987eb60218d1cb20747b39f
William Long II <wlong@bodhi.software>
2026-01-03 16:46:09 -0500

    Update SHA256 hashes for v1.2.1

--- Casks/claudelander.rb
+++ Casks/claudelander.rb
@@ -2,12 +2,12 @@ cask "claudelander" do
   version "1.2.1"
 
   on_intel do
-    sha256 "INTEL_SHA256_PLACEHOLDER"
+    sha256 "d23fb209fb8522f2eaecfd6b43419d4d1558b5c9fdab329a142327506de04c2f"
     url "https://github.com/William-Long-II/claudelander/releases/download/v#{version}/ClaudeLander-#{version}-mac.zip"
   end
 
   on_arm do
-    sha256 "ARM64_SHA256_PLACEHOLDER"
+    sha256 "ed8bf9aa29ab88c8d7f2e157bdd270e77677dd25db843191e1a7f3e676ce770b"
     url "https://github.com/William-Long-II/claudelander/releases/download/v#{version}/ClaudeLander-#{version}-arm64-mac.zip"
   end
 
